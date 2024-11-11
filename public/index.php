395d75ac08a08a1b81925742e88969f8e00bf56a
JosseJuventino <00048322@uca.edu.sv>
2024-11-11 04:13:49 -0600

    💩 Codigo a mejorar
    Codigo a mejorar sobre la landing

--- public/index.php
+++ public/index.php
@@ -2,10 +2,37 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Pagina principal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mercadito uca</title>   
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    <h1>Bienvenido a la pagina</h1>
-    <p>Esta es la página principal.</p>
+    <div class="container">
+        <header>
+            <div class="logo">Mercadito uca</div>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Ser emprendedor</a>
+                <a href="#">Sobre nosotros</a>
+                <a href="#">Negocios</a>
+            </nav>
+            <div class="buttons">
+                <a href="#">Iniciar Sesión</a>
+                <a href="#">Registrarse</a>
+            </div>
+        </header>
+        <section class="hero">
+            <h2>Bienvenidos a Nuestro Mercado</h2>
+            <p>Explora productos únicos de nuestros talentosos emprendedores. Únete a nuestra comunidad y disfruta de una experiencia de compra diferente, apoyando a pequeños negocios y proyectos apasionantes.</p>
+        </section>
+        <section class="image-section">
+            <img src="https://s.yimg.com/ny/api/res/1.2/v.f68LiPXMpBHylMgfrm6w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/es/tomatazos_56/e465e15bd5c67ae98920ef9d5c1e5e3b" alt="Anime Shop Logo">
+            <img src="https://s.yimg.com/ny/api/res/1.2/v.f68LiPXMpBHylMgfrm6w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/es/tomatazos_56/e465e15bd5c67ae98920ef9d5c1e5e3b" alt="Anime Shop Logo">
+            <img src="https://s.yimg.com/ny/api/res/1.2/v.f68LiPXMpBHylMgfrm6w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/es/tomatazos_56/e465e15bd5c67ae98920ef9d5c1e5e3b" alt="Anime Shop Logo">
+            <img src="https://s.yimg.com/ny/api/res/1.2/v.f68LiPXMpBHylMgfrm6w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/es/tomatazos_56/e465e15bd5c67ae98920ef9d5c1e5e3b" alt="Anime Shop Logo">
+            <img src="https://s.yimg.com/ny/api/res/1.2/v.f68LiPXMpBHylMgfrm6w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/es/tomatazos_56/e465e15bd5c67ae98920ef9d5c1e5e3b" alt="Anime Shop Logo">
+
+        </section>
+    </div>
 </body>
 </html>
